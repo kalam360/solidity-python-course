17ef2f2516b1ca1add28cb8a6dc82f7fa3bce83b
unknown <abulkalamfaruk@gmail.com>
2022-01-05 21:00:21 +0600

    removed artifacts

--- python-codes/config.py
+++ python-codes/config.py
@@ -1,3 +1,10 @@
 import os
+from dotenv import load_dotenv
 
 PWD = "c:/Users/PANOPTICON/Desktop/job-work/personal/solidity-python-course"
+load_dotenv(PWD + "/.env")
+
+chain_id = int(os.getenv("CHAIN_ID"))
+my_address = os.getenv("MY_ADDRESS")
+my_pk = os.getenv("MY_PK")
+rpc_port = os.getenv("RPC_PORT")
--- python-codes/deploy.py
+++ python-codes/deploy.py
@@ -1,8 +1,9 @@
-from config import PWD
-from solcx import compile_standard
-
 import os
 import json
+
+from config import PWD, chain_id, my_address, my_pk, rpc_port
+from solcx import compile_standard
+from web3 import Web3
 
 
 if not os.path.isdir(PWD + "/artifacts"):
@@ -32,3 +33,33 @@ compiled_sol = compile_standard(
 
 with open(artifacts_dir + "simple_storage.json", "w") as file:
     json.dump(compiled_sol, file)
+
+
+# get bytecode
+byte_code = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# web3 provider setup ganache
+w3 = Web3(Web3.HTTPProvider(f"http://127.0.0.1:{rpc_port}"))
+
+# create contract
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=byte_code)
+
+# get nonce from address transaction count
+nonce = w3.eth.getTransactionCount(my_address)
+gasPrice = 445051
+
+## Build and deploy contract
+# build transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce, "gasPrice": gasPrice}
+)
+
+# sign transaction
+signed_tx = w3.eth.account.sign_transaction(transaction, private_key=my_pk)
+
+# send transaction
